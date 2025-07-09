a574d1417ad4d2d69498374b1842275e25f8ceec
FlorinMarin-collab <florinvalentinmarin@gmail.com>
2025-07-09 11:39:34 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -35,16 +35,16 @@ export default function Home() {
     />
 
     {books.length > 0 ? (
-      <ul>
-        {filteredBooks.map((book) => (
-          <li key={book.id} style={{ marginBottom: '1rem' }}>
-            <strong>{book.title}</strong> by {book.author} ({book.year})
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <p>Loading books...</p>
-    )}
+  <ul>
+    {filteredBooks.map((book) => (
+      <li key={book.id} style={{ marginBottom: '1rem' }}>
+        <strong>{book.title}</strong> by {book.author} ({book.year})
+      </li>
+    ))}
+  </ul>
+) : (
+  <p>Loading books...</p>
+)}
   </div>
 );
 
